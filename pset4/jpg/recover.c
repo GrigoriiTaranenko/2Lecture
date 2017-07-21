d1b7ce8620ec56327ca1072676798e243dda8c8f
GRIGORIITARANENKO <grigorii.taranenko@gmail.com>
2017-07-21 18:24:50 +0300

    Исправил ввод данных

--- pset4/jpg/recover.c
+++ pset4/jpg/recover.c
@@ -8,6 +8,8 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
 #include "bmp.h"
 /**
@@ -18,11 +20,22 @@
 FILE *card;
 FILE *jpeg;
 
-bool checkPhoto(FILE *file);
+const BYTE chaos[] = {0xff, 0xd8, 0xff};
 
-void offsetByte(BYTE startByte[4]);
+bool createFile(int *numberJpeg);
 
-void 
+void swap(BYTE array[]);
+
+bool getByteBeforeJpeg(BYTE result[4]);
+
+bool checkBeginJpeg(BYTE arrayJpeg[512]);
+
+void firstWriteJPG(BYTE jpeg[4], BYTE arrayJpeg[512]);
+
+bool writeFile(BYTE arrayJpeg[512]);
+
+void offsetByte(BYTE arrayJpeg[512]);
+
 
 int main(int argc, char* argv[])
 {
@@ -32,42 +45,103 @@ int main(int argc, char* argv[])
     }
 
     card = fopen(argv[1], "r");
-    jpeg = fopen("mas.jpeg", "w");
 
-    if (card == null) {
+    if (card == NULL) {
+        printf("Неудалось открыть файл");
         return false;
     }
-
+    int numberJpeg = 1;
     BYTE arrayJpeg[512];
-    BYTE checkJpeg[3];
-    BYTE byt;
-    int i = 0;
-    int k = 1000000;
-    size_t result = fread(&byt, sizeof(BYTE), 1, card);
-    printf("byt = %d \n", byt);
-    while (k != 0  && result == sizeof(BYTE)) {
-        i++;
-    //    printf ("byt = %x \n", byt);
-        result = fread(&byt, sizeof(BYTE), 1, card);
-        if (byt == 255) {
-            k --;
-        }
+    BYTE checkJpeg[4];
+
+    if (!getByteBeforeJpeg(checkJpeg)) {
+        printf("Не найденно последовательности");
+        return 1;
     }
-    printf("i = %i байт \n", i);
-    printf("байт равен %x \n", byt);
-    fread(&byt, sizeof(BYTE), 1, card);
-    printf("байт равен %x \n", byt);
-    fread(&byt, sizeof(BYTE), 1, card);
-    printf("байт равен %x \n", byt);
-    fread(&byt, sizeof(BYTE), 1, card);
-    printf("байт равен %x \n", byt);
+    if (!createFile(&numberJpeg)) {
+        return 1;
+    }
+    firstWriteJPG(jpeg, arrayJpeg);
+
     fclose(card);
     return 0;
 }
 
-/*
-int x[10000];
-fread(x, 1000, 1, card);
-for (int i = 0; i<1000; i++) {
-printf("x = %x \n", x[i]);
-}*/
+/**
+ * Создания объекта для сохранения фотографии в формате jpeg
+ * @param numberJpeg - номер фото
+ * @return возвращает удачность создания файла
+ */
+bool createFile(int *numberJpeg) {
+    char *nameFile;
+    sprintf(nameFile, "%d", *numberJpeg);
+    strncat(nameFile, ".jpeg", 5);
+    jpeg = fopen(nameFile, "w");
+    if (jpeg == NULL) {
+        printf("Неудалось создать файл");
+        return false;
+    }
+    *numberJpeg++;
+    return true;
+}
+
+
+/**
+ * меняет местами значения элементов в массиве
+ * @param array
+ */
+void swap(BYTE array[]) {
+    for (int i = 0; i<2; i++) {
+        BYTE c = array[i];
+        array[i+1] = array[i];
+        array[i] = c;
+    }
+}
+
+/**
+ * нахождения первой последовательности до чтения jpeg
+ * @return bool При нахождении возвращает True при ненахождении возвращения false
+ * @var BYTE result[]
+ */
+bool getByteBeforeJpeg(BYTE result[4]) {
+    BYTE offset;
+    fread(&result, 4 * sizeof(BYTE), 1, card);
+    while (result == sizeof(BYTE) + 3) {
+        if (checkBeginJpeg(jpeg)) {
+            return true;
+        } else {
+            swap(result);
+            fread(&result[3], sizeof(BYTE), 1, jpeg);
+        }
+    }
+    return false;
+}
+
+bool checkBeginJpeg(BYTE arrayJpeg[512]) {
+    return (memcmp(arrayJpeg, chaos, 3) == 0 && arrayJpeg[3] > 223 && arrayJpeg[3] < 240);
+}
+
+/**
+ *
+ * @param int jpeg[4]
+ */
+void firstWriteJPG(BYTE jpeg[4], BYTE arrayJpeg[512]) {
+    fread(arrayJpeg, 508, 1, card);
+    offsetByte(arrayJpeg);
+    for (int i = 0; i < 4; i++) {
+        arrayJpeg[i] = jpeg[i];
+    }
+}
+
+bool writeFile(BYTE arrayJpeg[512]) {
+    if (checkBeginJpeg(arrayJpeg)) {
+        fclose(jpeg);
+    }
+}
+
+void offsetByte(BYTE arrayJpeg[512]) {
+    for (int i = 508; i>= 0; i--) {
+        arrayJpeg[i + 4] = arrayJpeg[i];
+    }
+}
+
